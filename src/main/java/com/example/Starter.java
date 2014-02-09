cbf799977a3d1a34aa0f1d59bf0391ee19587bb4
mingqin1 <mingqin@live.com>
2014-02-09 11:23:20 -0800

    updating

--- src/main/java/com/example/Starter.java
+++ src/main/java/com/example/Starter.java
@@ -9,26 +9,24 @@ import java.io.IOException;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.server.NetworkListener;
-
 import org.glassfish.grizzly.servlet.ServletRegistration;
 import org.glassfish.grizzly.servlet.WebappContext;
-//import org.glassfish.grizzly.servlet.ServletRegistration;
-//import org.glassfish.grizzly.servlet.WebappContext;
 import com.sun.jersey.spi.spring.container.servlet.SpringServlet;
 
 public class Starter {
 
     public static void main(String[] args) throws IOException {
         HttpServer server = new HttpServer();
-        NetworkListener listener = new NetworkListener("grizzly2", "localhost", 3388);
+        NetworkListener listener = new NetworkListener("grizzly2", "localhost", 2088);
         server.addListener(listener);
         WebappContext ctx = new WebappContext("ctx","/");
         final ServletRegistration reg = ctx.addServlet("spring", new SpringServlet());
-        reg.addMapping("/");
+        reg.addMapping("/*");
         ctx.addContextInitParameter("contextConfigLocation", "classpath:spring-context.xml");
         ctx.addListener("org.springframework.web.context.ContextLoaderListener");
         ctx.addListener("org.springframework.web.context.request.RequestContextListener");
         ctx.deploy(server);
+        
 
         server.start();
 
